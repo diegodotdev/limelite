d5cd095957e951ab73d2cd26d81309c47267388c
Diego Hernandez <diegohxxiv@gmail.com>
2024-02-19 11:51:04 -0800

    first commit

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,16 @@
+import MobileNav from "@/components/shared/mobile-nav";
+import Sidebar from "@/components/shared/sidebar";
+
+export default function Layout({ children }: { children: React.ReactNode }) {
+  return (
+    <main className="root">
+      {/* Sidebar */}
+      <Sidebar />
+      {/* Mobile Navigation */}
+      <MobileNav />
+      <div className="root-container">
+        <div className="wrapper">{children}</div>
+      </div>
+    </main>
+  );
+}
